c641d921d1cddb5add8d52004f47b50bc1a15147
Ivaylo Abadzhiev <IMAbadzhiev19@codingburgas.bg>
2023-07-05 11:43:45 +0300

    Add ApplicationDbContext.cs

---
+++ src/CVCreationPlatform.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Data.Models.CV;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+
+        public DbSet<Cv> Cvs { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<UserData> UsersData { get; set; }
+        public DbSet<UserEducation> UsersEducation { get; set; }
+        public DbSet<UserEmployment> UsersEmployments { get; set; }
+        public DbSet<UserPersonalDetails> UsersPersonalDetails { get; set; }
+    }
+}
